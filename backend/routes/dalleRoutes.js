e063a310daf83df742d3427f7f37ff8815cabd48
David Castro <davidcastrosi@hotmail.com>
2023-06-22 17:49:24 -0600

    fix openai leaked key

--- backend/routes/dalleRoutes.js
+++ backend/routes/dalleRoutes.js
@@ -7,7 +7,7 @@ dotenv.config()
 
 const router = express.Router()
 
-const configuration = new Configuration({ apiKey: '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' })
+const configuration = new Configuration({ apiKey: process.env.OPENAI_API_KEY })
 
 const openai = new OpenAIApi(configuration)
 
